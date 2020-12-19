6099ea673d1d6fd9e40be0314f20ab13167b6f10
Maksym <bms1200@gmail.com>
2020-12-19 09:50:52 +0200

    Update data.sh

--- lab3/data.sh
+++ lab3/data.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install httpd -y
-sleep 2
-sudo service start httpd
+sudo yum -y install httpd
+sudo service httpd start
+sudo chkconfig httpd on
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} +
+find /var/www -type f -exec sudo chmod 0664 {} +
+echo "<h1>Hello MB3 LAB</h1>" > /var/www/html/index.html
